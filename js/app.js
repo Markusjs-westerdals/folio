cceb517a132d255e10b64a267b9ca9962f1032aa
Markus Sørem <sormar@westerdals.no>
2016-02-16 22:46:51 +0100

    Got the site working, but it doesn't scroll good on mobile.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-    $('#fullpage').fullpage();
+    //$('.card').on('click', function () {
+    //    $(".dubi")
+    //        .animate(
+    //        {
+    //            padding: 0,
+    //            width: ['toggle', 'swing']
+    //        }, 'fast', 'easeOutExpo')
+    //});
+
+
+    $('#fullpage').fullpage({
+        css3: true,
+        scrollingSpeed: 500,
+        easingcss3: 'cubic-bezier(0.860, 0.000, 0.070, 1.000)',
+        controlArrows: false,
+        easing: 'easeInOutCirc',
+        loopHorizontal: false,
+        scrollOverflow: true,
+        //normalScrollElementTouchThreshold: 100,
+        //normalScrollElements: '.detail-content',
+
+    })
 });
